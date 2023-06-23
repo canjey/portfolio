2f2060f892a5dcd9c68a7e5a222ac097cc4d0b27
canjey <carolnjeri3235@gmail.com>
2023-06-23 20:43:46 +0300

    feat/Added responsiveness in My services component

--- src/Components/Homepage/myservices.js
+++ src/Components/Homepage/myservices.js
@@ -3,9 +3,9 @@ import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Profile from "../../assets/background1.png";
 import Interface from "../../interface.png";
-import Mobile from "../../mobile.png";
 import Graphics from "../../assets/graphics.png";
 import Button from '@mui/material/Button';
+import TerminalIcon from '@mui/icons-material/Terminal';
 
 const myImage = {
   backgroundImage: `url(${Profile})`,
@@ -20,40 +20,46 @@ export default function MyServices() {
   return (
     <>
       <Grid>
-        <Typography variant="h3" align="center" sx={{ marginTop: "40px" }}>
+        <Typography variant="h4" align="center" sx={{ marginTop: "40px" }}>
           {" "}
           My Services{" "}
         </Typography>
       </Grid>
       <Grid xs={5} sx={myImage}>
-        <Grid sx={{ margin: "5%",  }}>
+        <Grid>
           <Typography
             sx={{
-              marginTop: "30px",
+              marginTop: "3px",
               padding: "50px",
               marginLeft: "5%",
               marginRight: "15%"
             }}
           >
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industrys standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
+            A well-designed user interface will significantly increase conversions. 
+            I help you manage your business more effectively with a responsive design and top-notch sale funnels. 
+            Below are the services i offer my clients
           </Typography>
           <Grid container sx={{marginTop:'5%'}}>
-            <Grid item xs={4}>
-              <img src={Interface} width="200px" />
+            <Grid item xs={12} md={4} lg={3} sm={6} sx={{padding:'10px'}}>
+              <Typography align="center">
+              <img src={Interface} width="200px"  style={{marginLeft:{xs:'5%'}}}/>
+              </Typography>
               <Typography>User Interface Design</Typography>
               <Typography>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industrys standard dummy
-                text ever since the 1500s,
+              <li>Landing pages</li>
+              <li>User Flow</li>
+              <li>Wireframing</li>
+              <li>Prototyping</li>
+              <li>Mobile App Designs</li>
+               
               </Typography>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={12} md={4} lg={4} sm={6}>
+            <Typography align="center">
               <img src={Graphics} height="200px" />
+              </Typography>
               <Typography sx={{ color: "#F900D1" }}>
-                User Interface Design
+                Graphic Design
               </Typography>
               <Typography>
                 Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -61,23 +67,29 @@ export default function MyServices() {
                 text ever since the 1500s,
               </Typography>
             </Grid>
-            <Grid item xs={4}>
-              <img src={Mobile} width="200px" />
-              <Typography>Mobile Interface Design</Typography>
+            <Grid item xs={12} md={4} lg={4} sm={12} sx={{padding:'10px', textAlign:{xs:'center'}}}>
+            <Typography align="center">
+              <TerminalIcon fontSize="large" sx={{fontSize:'200px', color:"#BCBABA"}} />
+              </Typography>
+              <Typography >
+                Front-end Development</Typography>
               <Typography>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industrys standard dummy
-                text ever since the 1500s,
+              <li>HTML/CSS</li>
+              <li>JavaScript</li>
+              <li>WordPress</li>
+              <li> React.js </li>
+              <li>Node.js</li>
+              
               </Typography>
             </Grid>
           </Grid>
         </Grid>
         <Grid>
-        <Button variant="contained" sx={{backgroundColor:'#F900D1', width:'150px', marginLeft:'45%', borderRadius:'20px', }}>Hire Me</Button>
+        <Button variant="contained" sx={{backgroundColor:'#F900D1', marginTop:'5%',width:'150px', marginLeft:'40%', borderRadius:'20px', }}>Hire Me</Button>
         </Grid>
 
         <Grid>
-        <Typography variant="h3" align="center" sx={{ marginTop: "60px" }}>
+        <Typography variant="h4" align="center" sx={{ marginTop: "60px" }}>
           {" "}
          Portfolio{" "}
         </Typography>        
